86dc3b40eba979a449ba483e682b9a412424bba6
t-h-e-r-o-x <31180828+t-h-e-r-o-x@users.noreply.github.com>
2019-04-06 22:11:25 +0530

    TryingToWorkwithSocket

--- src/PlayQuiz.js
+++ src/PlayQuiz.js
@@ -5,10 +5,26 @@ class PlayQuiz extends Component {
   constructor(){
     super();
     this.state = {
-      
+      endpoint:"localhost:3000",
+      next: false
     }
   }
 
+  send = () => {
+    const socket = socketIOClient(this.state.endpoint);
+    socket.emit('stopquestion', this.state.next)
+  }
+
+setNext = () => {
+  this.setState({next: true});
+}
+
+render(){
+  const socket = socketIOClient(this.state.endpoint);
+  socket.on('stopquestion', (index) => {
+    
+  })
+}
 
 }
 
